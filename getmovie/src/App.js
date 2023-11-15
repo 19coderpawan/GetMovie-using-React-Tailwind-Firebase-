62b76cfff7c580c663764719d146a2135d50d331
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-11-15 18:49:57 +0530

    Update App.js

--- getmovie/src/App.js
+++ getmovie/src/App.js
@@ -1,11 +1,16 @@
+import AddMovieForm from "./Component/AddMovieForm";
 import Cards from "./Component/Cards";
 import Header from "./Component/Header";
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
       <Header/>
-      <Cards/>
+      <Routes>
+        <Route path="/" element={<Cards/>}/>
+        <Route path="/addmovie" element={<AddMovieForm/>}/>
+      </Routes>
+      
     </div>
   );
 }
