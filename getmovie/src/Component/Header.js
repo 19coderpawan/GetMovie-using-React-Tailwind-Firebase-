a2d04733addab1ce42389f0e2136a7bdb8a2a096
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-11-15 18:49:39 +0530

    Update Header.js

--- getmovie/src/Component/Header.js
+++ getmovie/src/Component/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
       <div className='text-3xl  font-bold p-3 border-b-2 border-white-500 flex justify-between items-center'>
-        <span>Get <span className='text-red-500'>Movie</span></span>
+      <Link to={'/'}>  <span>Get <span className='text-red-500'>Movie</span></span></Link>
+       <Link to={'/addmovie'}>
         <button className='text-lg hover:text-blue-400 hover:border-red-400
-        hover:border-2 p-2' >Add Movie <i class="fa-solid fa-plus"></i></button>
+        hover:border-2 p-2' >
+          Add Movie 
+          <i class="fa-solid fa-plus"></i>
+            </button>
+        </Link> 
+            
       </div>
     </>
   )
