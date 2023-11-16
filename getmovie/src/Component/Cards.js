91d80d31bae6b3b85ef2434b4fcb6d1f7d24f374
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-11-17 01:49:32 +0530

    Update Cards.js

--- getmovie/src/Component/Cards.js
+++ getmovie/src/Component/Cards.js
@@ -1,72 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactStars from 'react-stars'
+import { getDocs } from 'firebase/firestore'
+import {Movieref} from '../Firebase'
+import { ThreeCircles } from 'react-loader-spinner'
 const Cards = () => {
-    const [data, Setdata] = useState([
-        {
-            name: "Avengers Endgame",
-            Rating: 4.3,
-            Year: "2020",
-            img: "https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/4/-/4-4127_avengers_endgame_movie_poster_web.jpg"
-        },
-        {
-            name: "Captain America",
-            Rating: 4.3,
-            Year: "2011",
-            img: "https://static.displate.com/280x392/displate/2021-01-28/bce221eff0de2ed033e89311c45c805e_24f94cda8031146d198a8636239c55f5.jpg"
-        },
-        {
-            name: "Spider-Man No Way Home",
-            Rating: 4.3,
-            Year: "2021",
-            img: "https://i.redd.it/qct0dn6wtfu71.jpg"
-        },
-        {
-            name: "Venom",
-            Rating: 4.3,
-            Year: "2018",
-            img: "https://rukminim2.flixcart.com/image/850/1000/jsyyufk0/poster/w/r/v/medium-venom8-venom-movies-jumbo-poster-for-room-office-24-inch-original-imafed3dwgsqb9tj.jpeg?q=90"
-        },
-        {
-            name: "Captain-America Civil War",
-            Rating: 4.3,
-            Year: "2016",
-            img: "https://4.bp.blogspot.com/-h7pyEJl52vE/VXMNQmEC13I/AAAAAAAAIEE/QmBxbHPn2gc/s1600/CivilWarFanArt2.png"
-        }, {
-            name: "Avengers Endgame",
-            Rating: 4.3,
-            Year: "2020",
-            img: "https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/4/-/4-4127_avengers_endgame_movie_poster_web.jpg"
-        }, {
-            name: "Avengers Endgame",
-            Rating: 4.3,
-            Year: "2020",
-            img: "https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/4/-/4-4127_avengers_endgame_movie_poster_web.jpg"
-        }, {
-            name: "Avengers Endgame",
-            Rating: 4.3,
-            Year: "2020",
-            img: "https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/4/-/4-4127_avengers_endgame_movie_poster_web.jpg"
-        }, {
-            name: "Avengers Endgame",
-            Rating: 4.3,
-            Year: "2020",
-            img: "https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/4/-/4-4127_avengers_endgame_movie_poster_web.jpg"
-        }, {
-            name: "Avengers Endgame",
-            Rating: 4.3,
-            Year: "2020",
-            img: "https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/4/-/4-4127_avengers_endgame_movie_poster_web.jpg"
-        }
-    ])
+    const [data, Setdata] = useState([])
+    useEffect(()=>{
+       async function getdata(){
+        Setloading(true);
+        const fetcheddata=await getDocs(Movieref);
+         fetcheddata.forEach(doc => {
+            Setdata((pre)=>[...pre,doc.data()]);
+         });
+         Setloading(false)
+       }
+       getdata();
+    },[])
+    const [loading,Setloading]=useState(false);
     return (
         <>
             <div> <h1 className='text-center mt-3 font-bold font-serif'>Download all Movies for free Instantly!</h1> </div>
             <div className='flex flex-wrap justify-between p-3 mt-4    '>
-
-                {data.map((e, i) => {
+          {loading ?<div className=' w-full flex justify-center items-center '><ThreeCircles/></div>:
+                data.map((e, i) => {
                     return (
                         <div key={i} className='bg-slate-900 font-bold p-3  transition-all duration-700 border-2
-                rounded-xl  
+                rounded-xl m-3 
                 hover:-translate-y-4 mt-9'>
                             <img className='h-80 w-50  mt-1 cursor-pointer rounded-xl hover:border-2 
                     hover:border-white-200 ' src={e.img}
@@ -77,7 +36,7 @@ const Cards = () => {
                                     size={20}
                                     count={5}
                                     value={e.Rating}
-                                    edit={true}
+                                    edit={false}
                                     half={true} />
                             </h1>
                             <h1>Year:- <span>{e.Year}</span></h1>
@@ -90,3 +49,4 @@ const Cards = () => {
 }
 
 export default Cards
+
