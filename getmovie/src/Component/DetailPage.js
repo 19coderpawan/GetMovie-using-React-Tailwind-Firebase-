f1b516a75f2a2cf366c051e397893d2c09301133
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-11-18 02:03:30 +0530

    Update DetailPage.js

--- getmovie/src/Component/DetailPage.js
+++ getmovie/src/Component/DetailPage.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactStars from 'react-stars'
+/*The useParams hook returns an object of key/value pairs of the dynamic params from the current URL 
+that were matched by the <Route path>. 
+Child routes inherit all params from their parent routes.*/
+import { useParams } from 'react-router-dom'
+import { db } from '../Firebase'
+import { getDoc,doc } from 'firebase/firestore'
 const DetailPage = () => {
+    const{id}=useParams();
+    const [data,Setdata]=useState({
+        name:"",
+        Year:"",
+        Rating:"",
+        Description:"",
+        img:""
+    });
+    useEffect(()=>{
+      async function getdata(){
+        const fetcheddata=await doc(db,"movie",id);
+        const displayfetchdata= await getDoc(fetcheddata);
+        Setdata(displayfetchdata.data());
+      }
+      getdata();
+    },[])
   return (
     <>
       <div className='flex flex-col md:flex-row md:items-start items-center w-full justify-center mt-4 p-3'>
-        <img className='h-96 md:w-80  md:sticky top-4' src='https://static.displate.com/280x392/displate/2021-01-28/bce221eff0de2ed033e89311c45c805e_24f94cda8031146d198a8636239c55f5.jpg'/>
+        <img className='h-96 md:w-80  md:sticky top-4' src={data.img}/>
         <div className='ml-3 w-full  md:w-1/2'>
-            <h1 className='text-3xl font-bold text-gray-400'>Captain America <span className='text-xl'>(2011)</span></h1>
+            <h1 className='text-3xl font-bold text-gray-400'>{data.name} <span className='text-xl'>({data.Year})</span></h1>
             <ReactStars
             size={25}
             count={5}
-            value={4}
+            value={data.Rating}
             edit={false}
             half={true} />
-            <p>Starring Matt Salinger as Steve Rogers, the film follows Captain America as he faces the villainous Red Skull (Scott Paulin) during the Second World War, becomes frozen in ice, and is revived decades later to face Red Skull again.
-            Starring Matt Salinger as Steve Rogers, the film follows Captain America as he faces the villainous Red Skull (Scott Paulin) during the Second World War, becomes frozen in ice, and is revived decades later to face Red Skull again.
-            </p>
-            <p>Starring Matt Salinger as Steve Rogers, the film follows Captain America as he faces the villainous Red Skull (Scott Paulin) during the Second World War, becomes frozen in ice, and is revived decades later to face Red Skull again.
-            Starring Matt Salinger as Steve Rogers, the film follows Captain America as he faces the villainous Red Skull (Scott Paulin) during the Second World War, becomes frozen in ice, and is revived decades later to face Red Skull again.
-            </p>
+            <p>{data.Description}</p>
             
         </div>
       </div>
