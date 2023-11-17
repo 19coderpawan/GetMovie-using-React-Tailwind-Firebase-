483cd454fa03245639f50cc3757ae0a3f7cec555
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-11-18 02:04:05 +0530

    Created Route for the Detail page

--- getmovie/src/Component/Cards.js
+++ getmovie/src/Component/Cards.js
@@ -3,6 +3,7 @@ import ReactStars from 'react-stars'
 import { getDocs } from 'firebase/firestore'
 import {Movieref} from '../Firebase'
 import { ThreeCircles } from 'react-loader-spinner'
+import {Link} from 'react-router-dom'
 const Cards = () => {
     const [data, Setdata] = useState([])
     useEffect(()=>{
@@ -20,14 +21,15 @@ const Cards = () => {
     return (
         <>
             <div> <h1 className='text-center mt-3 font-bold font-serif'>Download all Movies for free Instantly!</h1> </div>
-            <div className='flex flex-wrap justify-between p-3 mt-4    '>
+            <div className='flex flex-wrap  p-3 mt-4 justify-between '>
           {loading ?<div className=' w-full flex justify-center items-center '><ThreeCircles/></div>:
                 data.map((e, i) => {
                     return (
-                        <div key={i} className='bg-slate-900 font-bold p-3  transition-all duration-700 border-2
+                 <Link to={`/detail/${e.id}`}>
+                 <div key={i} className='bg-slate-900 font-bold p-3  transition-all duration-700 border-2
                 rounded-xl m-3 
                 hover:-translate-y-4 mt-9'>
-                            <img className='h-80 w-50  mt-1 cursor-pointer rounded-xl hover:border-2 
+                            <img className=' h-70 w-60  mt-1 cursor-pointer rounded-xl hover:border-2 
                     hover:border-white-200 ' src={e.img}
                                 alt="img" />
                             <h1 >Name:- <span className='truncate text-sm'>{e.name}</span></h1>
@@ -41,6 +43,7 @@ const Cards = () => {
                             </h1>
                             <h1>Year:- <span>{e.Year}</span></h1>
                         </div>
+                 </Link>       
                     )
                 })}
             </div>
@@ -49,4 +52,3 @@ const Cards = () => {
 }
 
 export default Cards
-
