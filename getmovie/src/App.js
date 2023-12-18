fc5b46ae33f0ff4c13c8da2d8ed3267d29042d13
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-12-19 00:38:27 +0530

    Update App.js

--- getmovie/src/App.js
+++ getmovie/src/App.js
@@ -1,18 +1,31 @@
 import AddMovieForm from "./Component/AddMovieForm";
 import Cards from "./Component/Cards";
+import DetailPage from "./Component/DetailPage";
 import Header from "./Component/Header";
 import { Route, Routes } from "react-router-dom";
+import Login from "./Component/Login";
+import Signup from "./Component/Signup";
+import { createContext, useState } from "react";
+
+const Appstate=createContext();
 function App() {
+  const [login,Setlogin]=useState(false);
+
   return (
+    <Appstate.Provider value={(login, Setlogin)}>
     <div>
       <Header/>
       <Routes>
         <Route path="/" element={<Cards/>}/>
         <Route path="/addmovie" element={<AddMovieForm/>}/>
+        <Route path="/detail/:id" element={<DetailPage/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/Signup" element={<Signup/>}/>
       </Routes>
-      
     </div>
+    </Appstate.Provider>
   );
 }
 
 export default App;
+export {Appstate};
