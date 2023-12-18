f0cc57c12992cccc3506b41825f48edb11360984
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-12-19 00:38:48 +0530

    Update Header.js

--- getmovie/src/Component/Header.js
+++ getmovie/src/Component/Header.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { Appstate } from '../App'
 
 const Header = () => {
+  const Appstateuse=useContext(Appstate);
   return (
     <>
       <div className='text-3xl  font-bold p-3 border-b-2 border-white-500 flex justify-between items-center'>
-      <Link to={'/'}>  <span>Get <span className='text-red-500'>Movie</span></span></Link>
+      <Link to={'/'}>  <span>Get <span className='text-red-500'>Movies</span></span></Link>
+      {Appstateuse.login ?
        <Link to={'/addmovie'}>
         <button className='text-lg hover:text-blue-400 hover:border-red-400
         hover:border-2 p-2' >
-          Add Movie 
-          <i class="fa-solid fa-plus"></i>
+          Add Movies <i class="fa-solid fa-plus"></i>
             </button>
         </Link> 
-            
+        :
+        <Link to={'/Login'}>
+        <button className='text-lg hover:text-blue-400 hover:border-red-400
+        hover:border-2 p-2' >
+          Login 
+            </button>
+        </Link> 
+      }
       </div>
     </>
   )
