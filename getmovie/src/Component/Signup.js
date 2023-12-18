1f50ef6c370f05331a4e6ed172df81022ca007e9
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-12-19 01:15:40 +0530

    Update Signup.js

--- getmovie/src/Component/Signup.js
+++ getmovie/src/Component/Signup.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import { Auth } from '../Firebase'
+import { TailSpin } from 'react-loader-spinner'
+import swal from 'sweetalert'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 
 const Signup = () => {
@@ -9,6 +12,42 @@ const Signup = () => {
     email:"",
     password:""
    })
+   const [Loading,SetLoading]=useState(false);
+
+   const handleSubmit=async(e)=>{
+     e.preventDefault();
+     if(!Signupdata.name || !Signupdata.email || !Signupdata.password){
+    swal({
+        title:"Fill out all the details Please!",
+        icon:"error",
+        buttons:false,
+        timer:2000
+    })
+    return;
+     }
+
+     try {
+        SetLoading(true);
+        await createUserWithEmailAndPassword(Auth,Signupdata.email,Signupdata.password);
+        swal({
+            title:"Your are Registered Thank you for Joining Us!",
+            icon:"success",
+            buttons:false,
+            timer:2000
+        })
+
+     } catch (error) {
+         SetLoading(false);
+         swal({
+            title:"Enable to Register Try again !",
+            icon:"error",
+            button:false,
+            timer:2000
+         })
+     }
+     SetLoading(false);
+     SetSignupdata({...Signupdata,name:"",email:"",password:""})
+   }
 
     return (
         <section class="bg-gray-1000">
@@ -18,7 +57,7 @@ const Signup = () => {
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Register Yourself
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
+                        <form class="space-y-4 md:space-y-6" action="" onSubmit={handleSubmit}>
                             <div>
                                 <label for="email" class="block mb-2 text-sm font-medium text-white-900 ">Your name</label>
                                 <input type="name" value={Signupdata.name} name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
@@ -34,17 +73,10 @@ const Signup = () => {
                                 <input type="password" value={Signupdata.password} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                                 onChange={(e)=>{SetSignupdata({...Signupdata,password:e.target.value})}} required="" />
                             </div>
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                                    </div>
-                                    <div class="ml-3 text-sm">
-                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <button type="submit" class="w-full text-white border-2 hover:border-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Signup</button>
+                            
+                            <button type="submit" class="w-full text-white border-2 hover:border-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                {Loading ? <div className=' w-full flex justify-center items-center '><TailSpin  height={30} color='red' width={70}/></div>:"Signup"}
+                            </button>
                             <p class="text-sm font-light text-white-500 dark:text-white-400">
                                 Already have a Account ? <Link to={'/Login'} class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</Link>
                             </p>
