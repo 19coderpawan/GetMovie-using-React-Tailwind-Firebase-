484bceb323bf4d47c2c632c527cf80fe7677c391
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-12-23 22:07:38 +0530

    Update Login.js

--- getmovie/src/Component/Login.js
+++ getmovie/src/Component/Login.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import swal from 'sweetalert'
 import { TailSpin } from 'react-loader-spinner'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { Auth } from '../Firebase'
 
 const Login = () => {
 
@@ -9,6 +11,32 @@ const Login = () => {
         email:'',
         password:''
     })
+    const[Loading,SetLoading]=useState(false);
+    const handleLoginSubmit=async(e)=>{
+        e.preventDefault();
+        try {
+            SetLoading(true);
+            await signInWithEmailAndPassword(Auth,logindata.email,logindata.password);
+            swal({
+                title:"Welcome to Movie Verse!",
+                icon:"success",
+                buttons:false,
+                timer:2000
+            })
+            
+        } catch (error) {
+            SetLoading(false);
+            swal({
+                title:"Check Your Credentials",
+                icon:"error",
+                buttons:false,
+                timer:2000
+            })
+            console.log(error);
+        }
+        Setlogindata({...Setlogindata,email:"",password:""});
+        SetLoading(false);
+    }
 
     return (
         <section class="bg-gray-1000">
@@ -18,7 +46,7 @@ const Login = () => {
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Login to your account
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
+                        <form class="space-y-4 md:space-y-6" action="#" onSubmit={handleLoginSubmit}>
                             <div>
                                 <label for="email" class="block mb-2 text-sm font-medium text-white-900 ">Your email</label>
                                 <input type="email" value={logindata.email}  name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
@@ -29,18 +57,10 @@ const Login = () => {
                                 <input type="password" value={logindata.password} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                                 onChange={(e)=>{Setlogindata({...logindata,password:e.target.value})}}  required="" />
                             </div>
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                                    </div>
-                                    <div class="ml-3 text-sm">
-                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                                    </div>
-                                </div>
-                                <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                            </div>
-                            <button type="submit" class="w-full text-white border-2 hover:border-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
+                            
+
+                            <button type="submit" class="w-full text-white border-2 hover:border-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            > {Loading?<div className=' w-full flex justify-center items-center '><TailSpin  height={30} color='green' width={70}/></div>:"Login"} </button>
                             <p class="text-sm font-light text-white-500 dark:text-white-400">
                                 Don’t have an account yet? <Link to={'/Signup'} class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
                             </p>
