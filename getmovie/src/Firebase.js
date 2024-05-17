687dd8569d59d91790f06d08a79064f0e2feac0d
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2024-05-18 03:37:11 +0530

    Update Firebase.js

--- getmovie/src/Firebase.js
+++ getmovie/src/Firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDDL8siXCj0STAj4FzxDVdk4UoQRFp30nA",
-  authDomain: "getmovies-9eded.firebaseapp.com",
-  projectId: "getmovies-9eded",
-  storageBucket: "getmovies-9eded.appspot.com",
-  messagingSenderId: "244831132081",
-  appId: "1:244831132081:web:97b226282ce0cff6780590"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
